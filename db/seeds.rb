4571bff662b331111ab1cfebbf3643e5167590e9
Emily Baird <emilym.baird@gmail.com>
2022-01-19 22:55:54 +0000

    index begun

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!([
+  { name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3 },
+  { name: 'Bright and Modern Flat Central Canary Wharf',
+    address: '37 Marsh Wall',
+    description: 'Easy commute to the banks and centrally located along major train lines',
+    price_per_night: 103,
+    number_of_guests: 2 },
+  { name: 'Homely Flat in Central London',
+    address: '31 Kensington Gardens',
+    description: 'Centrally located near important tourist destinations',
+    price_per_night: 140,
+    number_of_guests: 2 },
+  { name: 'Homely Flat in Greenwhich',
+    address: '22 Shipwright St',
+    description: 'Gorgeous area, lots of fun museums and local restaurants',
+    price_per_night: 84,
+    number_of_guests: 3 }
+])
