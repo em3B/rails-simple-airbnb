abfc3f6944c8a67b6c214e7bf7ec098ab2461ebd
Emily Baird <emilym.baird@gmail.com>
2022-01-20 13:33:00 +0000

    css added for index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :flats
+  get 'destroy', to: 'flats#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
